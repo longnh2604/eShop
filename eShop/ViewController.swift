912a7e70dfafc6e48232cfe38d220d580974d5a2
oluxe_dev@oluxe.co.jp <oluxe_dev@oluxe.co.jp>
2020-07-10 16:04:18 +0900

    update pod
    update pod

---
+++ eShop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eShop
+//
+//  Created by Oluxe on 2020/07/10.
+//  Copyright © 2020 Long. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
