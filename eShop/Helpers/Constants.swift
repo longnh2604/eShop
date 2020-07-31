a2765a5de00fac2c71b0280a91f508e0fb8357e9
oluxe_dev@oluxe.co.jp <oluxe_dev@oluxe.co.jp>
2020-07-31 18:09:21 +0900

    Update Item class
    Update Item class

--- eShop/Helpers/Constants.swift
+++ eShop/Helpers/Constants.swift
@@ -18,3 +18,9 @@ public let kBASKET_PATH = "Basket"
 public let kNAME = "name"
 public let kIMAGENAME = "imageName"
 public let kOBJECTID = "objectId"
+
+//Item
+public let kCATEGORYID = "categoryId"
+public let kDESCRIPTION = "description"
+public let kPRICE = "price"
+public let kIMAGELINKS = "imageLinks"
---
+++ eShop/Models/Item.swift
@@ -0,0 +1,44 @@
+//
+//  Item.swift
+//  eShop
+//
+//  Created by Oluxe on 2020/07/31.
+//  Copyright © 2020 Long. All rights reserved.
+//
+
+import UIKit
+
+class Item {
+    
+    var id: String!
+    var categoryId: String!
+    var name: String!
+    var description: String!
+    var price: Double!
+    var imageLinks: [String]!
+    
+    init() {
+    }
+    
+    init(_dictionary: NSDictionary) {
+        
+        id = _dictionary[kOBJECTID] as? String
+        categoryId = _dictionary[kCATEGORYID] as? String
+        name = _dictionary[kNAME] as? String
+        description = _dictionary[kDESCRIPTION] as? String
+        price = _dictionary[kPRICE] as? Double
+        imageLinks = _dictionary[kIMAGELINKS] as? [String]
+    }
+}
+
+//MARK: Save items func
+
+func saveItemToFirestore(_ item: Item) {
+    FirebaseReference(.Items).document(item.id).setData(itemDictionaryFrom(item) as! [String : Any])
+}
+
+//MARK: Helper functions
+
+func itemDictionaryFrom(_ item: Item) -> NSDictionary {
+    return NSDictionary(objects: [item.id, item.categoryId, item.name, item.description, item.price, item.imageLinks], forKeys: [kOBJECTID as NSCopying, kCATEGORYID as NSCopying, kNAME as NSCopying, kDESCRIPTION as NSCopying, kPRICE as NSCopying, kIMAGELINKS as NSCopying])
+}
